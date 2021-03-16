3003ca67d871061b531aeb01e4379a4a7ff9807e
Mirc224 <mircus224@gmail.com>
2021-03-16 18:15:27 +0100

    Add project files.

---
+++ Csharp_data_structures/DataStructures/PairingHeap/PriorityHeap.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csharp_data_structures.DataStructures.PairingHeap
+{
+    class PriorityHeap<K, P, V>
+        where K : IComparable
+        where P : IComparable
+    {
+        public K Peek { get => _root != null ? _root.Key : default(K); }
+        private PriorityHeapNode<K, P, V> _root = null;
+        private int _numberOfNodes = 0;
+        public int Count { get => _numberOfNodes; }
+
+        public void Insert(K key, P priority, V value)
+        {
+            var newNode = new PriorityHeapNode<K, P, V>(key, priority,  value);
+            ++_numberOfNodes;
+            if (_root == null)
+            {
+                _root = newNode;
+                return;
+            }
+            _root = Pair(_root, newNode);
+
+        }
+
+        private PriorityHeapNode<K, P, V> Pair(PriorityHeapNode<K, P, V> nodeOne, PriorityHeapNode<K, P, V> nodeTwo)
+        {
+            if (nodeOne.Key.CompareTo(nodeTwo.Key) == 0)
+            {
+                if (nodeOne.Priority.CompareTo(nodeTwo.Priority) > 0)
+                {
+                    var leftSon = nodeOne.LeftSon;
+                    nodeOne.LeftSon = nodeTwo;
+                    nodeTwo.RightSon = leftSon;
+                    return nodeOne;
+                }
+                else
+                {
+                    var leftSon = nodeTwo.LeftSon;
+                    nodeTwo.LeftSon = nodeOne;
+                    nodeOne.RightSon = leftSon;
+                    return nodeTwo;
+                }
+            }
+            else if (nodeOne.Key.CompareTo(nodeTwo.Key) < 0)
+            {
+                var leftSon = nodeOne.LeftSon;
+                nodeOne.LeftSon = nodeTwo;
+                nodeTwo.RightSon = leftSon;
+                return nodeOne;
+            }
+            else
+            {
+                var leftSon = nodeTwo.LeftSon;
+                nodeTwo.LeftSon = nodeOne;
+                nodeOne.RightSon = leftSon;
+                return nodeTwo;
+            }
+        }
+
+        public V GetMin()
+        {
+            var minimalNode = _root;
+            var front = new Queue<PriorityHeapNode<K, P, V>>();
+            PriorityHeapNode<K, P, V> prevNode = null;
+            var tmpNode1 = _root.LeftSon;
+            while (tmpNode1 != null)
+            {
+                front.Enqueue(tmpNode1);
+                prevNode = tmpNode1;
+                tmpNode1 = tmpNode1.RightSon;
+                prevNode.RightSon = null;
+            }
+
+            if (front.Count == 0)
+                _root = null;
+            else
+            {
+                PriorityHeapNode<K, P, V> tmpNode2 = null;
+                while (front.Count > 1)
+                {
+                    tmpNode1 = front.Dequeue();
+                    tmpNode2 = front.Dequeue();
+                    front.Enqueue(Pair(tmpNode1, tmpNode2));
+                }
+                _root = front.Dequeue();
+            }
+            --_numberOfNodes;
+            return minimalNode.Value;
+        }
+
+        public void Reset()
+        {
+            _numberOfNodes = 0;
+            _root = null;
+        }
+
+
+
+
+        private class PriorityHeapNode<K, P, V>
+        where K : IComparable
+        where P : IComparable
+
+        {
+            public K Key { get; set; }
+            public V Value { get; set; }
+            public P Priority { get; set; }
+            public PriorityHeapNode<K, P, V> LeftSon { get; set; }
+            public PriorityHeapNode<K, P, V> RightSon { get; set; }
+            public PriorityHeapNode(K key, P priority, V value)
+            {
+                Key = key;
+                Value = value;
+                Priority = priority;
+            }
+        }
+    }
+}
---
+++ Csharp_data_structures/Program.cs
@@ -0,0 +1,92 @@
+﻿using Csharp_data_structures.DataStructures.PairingHeap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Csharp_data_structures
+{
+    class Program
+    {
+        class TMPClass : IComparable
+        {
+            public int CompareTo(object obj)
+            {
+                return 0;
+            }
+        }
+        static void Main(string[] args)
+        {
+            //var tmpHeap = new PriorityHeap<int, int, string>();
+            var tmpHeap = new PairingHeap<int, int>();
+            var realPriority = new List<int>();
+            /*int cislo1 = -1;
+            int cislo2 = -1;
+            var gen = new Random();
+            for(int i = 0; i < 50; ++i)
+            {
+                cislo1 = gen.Next(10);
+                cislo2 = gen.Next(3);
+                tmpHeap.Insert(cislo1, cislo2, $"Cislo: {cislo1} Priorita: {cislo2}");
+            }*/
+
+            /*while (tmpHeap.Count != 0)
+            {
+                Console.WriteLine(tmpHeap.GetMin());
+            }
+*/
+            /*if(tmpHeap.Peek == null)
+            {
+                Console.WriteLine("Je to null");
+            }
+            Console.WriteLine(tmpHeap.Peek);*/
+            Random gen = new Random();
+            int numberOfTests = 100000;
+
+            double probabilityOfInsert = 0.5;
+
+            int cislo = -1;
+            int inserts = 0;
+            int geters = 0;
+            for (int i = 0; i < numberOfTests; ++i)
+            {
+                if (gen.NextDouble() < probabilityOfInsert)
+                {
+                    cislo = gen.Next(100);
+                    tmpHeap.Insert(cislo, cislo);
+                    realPriority.Add(cislo);
+                    ++inserts;
+                }
+                else
+                {
+                    if (realPriority.Count != 0)
+                    {
+                        ++geters;
+                        int minimal = realPriority.Min();
+                        if (minimal != tmpHeap.GetMin())
+                        {
+                            Console.WriteLine("Chyba!");
+                            return;
+                        }
+                        realPriority.Remove(minimal);
+                    }
+                }
+            }
+            Console.WriteLine($"Inserts: {inserts}  Geters: {geters}");
+            /*realPriority.Sort();
+            foreach (var item in realPriority)
+            {
+                Console.WriteLine(item);
+            }
+            *//*
+            if(realPriority.Count != tmpHeap.Count)
+            {
+                Console.WriteLine("Chyba!");
+            }*/
+            /*Console.WriteLine("Moj heap");
+            while(tmpHeap.Count != 0)
+            {
+                Console.WriteLine(tmpHeap.GetMin());
+            }*/
+        }
+    }
+}
